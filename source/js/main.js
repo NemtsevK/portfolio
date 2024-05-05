982e04e4a1b7b1bf80b44f6708919bd485c3b339
Konstantin <mutecross@gmail.com>
2024-05-05 12:13:47 +0300

    Добавлена кнопка открытия меню в мобильной версии

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,38 @@
+const page = document.querySelector('.page');
+const nav = document.querySelector('.nav');
+const toggleTheme = page.querySelector('.header__toggle-theme');
+const toggleThemeInput = page.querySelector('.header__toggle-theme-checkbox');
+const toggleNav = page.querySelector('.header__toggle-nav');
+const toggleNavInput = page.querySelector('.header__toggle-nav-checkbox');
+
 const button = document.querySelector('.works__button');
 const worksItems = document.querySelectorAll('.works__item--hide');
+const navLinks = nav.querySelectorAll('.nav__link');
+
+const onToggleNavClick = (event) => {
+  nav.classList.toggle('nav--opened');
+  if (event.target.checked) {
+    toggleNav.setAttribute('aria-label', 'Закрыть меню');
+    toggleNav.setAttribute('title', 'Закрыть меню');
+  } else {
+    toggleNav.setAttribute('aria-label', 'Открыть меню');
+    toggleNav.setAttribute('title', 'Открыть меню');
+  }
+}
+
+const onNavLinkClick = () => {
+  nav.classList.toggle('nav--opened');
+  toggleNav.checked = false;
+}
+
+page.classList.remove('page--nojs');
+toggleNavInput.addEventListener('click', onToggleNavClick);
+navLinks.forEach(navLink => navLink.addEventListener('click', onNavLinkClick));
 
 let showItems = false;
 
 const onButtonShowClick = () => {
-
-  worksItems.forEach(function (item) {
-    item.classList.toggle('works__item--hide');
-  })
-
+  worksItems.forEach(item => item.classList.toggle('works__item--hide'));
   showItems = (showItems === false);
   button.textContent = (showItems === true) ? 'Скрыть' : 'Показать больше';
 }
