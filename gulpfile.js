0bd46d866b932c5c4965476f33638b5722a0431d
Konstantin <mutecross@gmail.com>
2024-05-27 23:01:22 +0300

    Обновление библиотек и изменение файла сборки

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+import { readFileSync, rmSync } from 'node:fs';
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
@@ -5,161 +6,180 @@ import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import csso from 'postcss-csso';
 import rename from 'gulp-rename';
-import htmlmin from 'gulp-htmlmin';
 import terser from 'gulp-terser';
-import squoosh from 'gulp-libsquoosh';
+import webp from 'gulp-webp';
+import imagemin from 'gulp-imagemin';
 import svgo from 'gulp-svgmin';
-import del from 'del';
 import browser from 'browser-sync';
 import replace from 'gulp-replace';
-import {stacksvg} from 'gulp-stacksvg';
+import { stacksvg } from 'gulp-stacksvg';
 import dayjs from 'dayjs';
 
-export const styles = () => {
-  return gulp.src('source/sass/style.scss', {sourcemaps: true})
+const { src, dest, watch, series, parallel } = gulp;
+
+const SOURCE_ROOT = 'source';
+const BUILD_ROOT = 'build';
+const PATH_TO_MARKUP = `${SOURCE_ROOT}/**/*.html`;
+const PATH_TO_STYLE = `${SOURCE_ROOT}/sass/style.scss`;
+const PATH_TO_STYLES = `${SOURCE_ROOT}/sass/**/*.scss`;
+const PATH_TO_SCRIPTS = `${SOURCE_ROOT}/js/*.js`;
+const PATH_TO_RASTER = `${SOURCE_ROOT}/images/**/*.{png,jpg}`;
+const PATH_TO_SPRITES = `${SOURCE_ROOT}/images/sprites/*.svg`;
+const PATHS_TO_WEBP = [PATH_TO_RASTER, `!${SOURCE_ROOT}/images/favicons/*.png`];
+const PATHS_TO_VECTOR = `${SOURCE_ROOT}/images/**/*.svg`;
+const PATHS_TO_FILES = [
+  `${SOURCE_ROOT}/fonts/**/*.woff2`,
+  `${SOURCE_ROOT}/*.ico`,
+  `${SOURCE_ROOT}/*.webmanifest`,
+];
+const RESULT_TO_IMAGES = `${BUILD_ROOT}/images`;
+
+const currentDateTimeString = dayjs().format('YYYYMMDDHHmmss');
+
+const processMarkup = () => {
+  return src(PATH_TO_MARKUP)
+    .pipe(replace('.css', `.min.css?${currentDateTimeString}`))
+    .pipe(replace('.js', `.min.js?${currentDateTimeString}`))
+    .pipe(dest(BUILD_ROOT));
+};
+
+export const processStyles = () => {
+  return src(PATH_TO_STYLE, { sourcemaps: true })
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
       autoprefixer(),
       csso()
     ]))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('build/css', {sourcemaps: '.'}))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest(`${BUILD_ROOT}/css`, { sourcemaps: '.' }))
     .pipe(browser.stream());
 };
 
-const scripts = () => {
-  return gulp.src('source/js/*.js')
+const processScripts = () => {
+  return src(PATH_TO_SCRIPTS)
     .pipe(terser())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('build/js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest(`${BUILD_ROOT}/js`))
     .pipe(browser.stream());
 };
 
-const currentDateTimeString = dayjs().format('YYYYMMDDHHmmss');
-
-const html = () => {
-  return gulp.src('source/*.html')
-    .pipe(replace('.css', `.min.css?${currentDateTimeString}`))
-    .pipe(replace('.js', `.min.js?${currentDateTimeString}`))
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('build'));
-};
-
-const optimizeImages = () => {
-  return gulp.src('source/images/**/*.{png,jpg}')
-    .pipe(squoosh())
-    .pipe(gulp.dest('build/images'));
+const optimizeRaster = () => {
+  return src(PATH_TO_RASTER, { encoding: false })
+    .pipe(imagemin())
+    .pipe(dest(RESULT_TO_IMAGES));
 };
 
 const copyImages = () => {
-  return gulp.src('source/images/**/*.{png,jpg}')
-    .pipe(gulp.dest('build/images'));
+  return src(PATH_TO_RASTER, { encoding: false })
+    .pipe(dest(RESULT_TO_IMAGES));
 };
 
 const createWebp = () => {
-  return gulp.src(['source/images/**/*.{png,jpg}', '!source/images/favicons/*.png'])
-    .pipe(squoosh({
-      webp: {}
-    }))
-    .pipe(gulp.dest('build/images'));
+  return src(PATHS_TO_WEBP, { encoding: false })
+    .pipe(webp())
+    .pipe(dest(RESULT_TO_IMAGES));
 };
 
-const optimizeSvg = () => {
-  return gulp.src(['source/images/**/*.svg', '!source/images/sprites/*.svg'])
+const optimizeVector = () => {
+  return src([PATHS_TO_VECTOR, `!${PATH_TO_SPRITES}`])
     .pipe(svgo())
-    .pipe(gulp.dest('build/images'));
+    .pipe(dest(RESULT_TO_IMAGES));
 };
 
 const createStack = () => {
-  return gulp.src('source/images/sprites/*.svg')
+  return src(PATH_TO_SPRITES)
     .pipe(svgo())
-    .pipe(stacksvg({output: 'sprite.svg'}))
-    .pipe(gulp.dest('build/images'));
+    .pipe(stacksvg({ output: 'sprite.svg' }))
+    .pipe(dest(RESULT_TO_IMAGES));
 };
 
-const copy = (done) => {
-  gulp.src([
-    'source/fonts/**/*.woff2',
-    'source/*.ico',
-    'source/*.webmanifest',
-  ], {
-    base: 'source'
-  })
-    .pipe(gulp.dest('build'));
+const copyFiles = (done) => {
+  src(PATHS_TO_FILES, { base: SOURCE_ROOT })
+    .pipe(dest(BUILD_ROOT));
   done();
 };
 
-const clean = () => del('build');
-
-const server = (done) => {
-  browser.init({
-    server: {
-      baseDir: 'build'
-    },
-    cors: true,
-    notify: false,
-    ui: false,
+const removeBuild = (done) => {
+  rmSync(BUILD_ROOT, {
+    force: true,
+    recursive: true,
   });
   done();
-};
+}
 
-const reload = (done) => {
+const reloadServer = (done) => {
   browser.reload();
   done();
 };
 
-const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/js/*.js', gulp.series(scripts));
-  gulp.watch('source/*.html', gulp.series(html, reload));
-  gulp.watch(
-    'source/images/**/*.{png,jpg}',
-    gulp.series(
+const startServer = () => {
+  browser.init({
+    server: {
+      baseDir: BUILD_ROOT
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  }, (err, bs) => {
+    bs.addMiddleware('*', (req, res) => {
+      res.write(readFileSync(`${BUILD_ROOT}/404.html`));
+      res.end();
+    });
+  });
+
+  watch(PATH_TO_STYLES, series(processStyles));
+  watch(PATH_TO_SCRIPTS, series(processScripts));
+  watch(PATH_TO_MARKUP, series(processMarkup, reloadServer));
+  watch(
+    PATH_TO_RASTER,
+    series(
       copyImages,
       createWebp,
-      styles,
-      html,
-      reload,
+      processMarkup,
+      reloadServer,
     )
   );
-  gulp.watch(
-    'source/images/**/*.svg',
-    gulp.series(
-      optimizeSvg,
+  watch(
+    PATHS_TO_VECTOR,
+    series(
+      optimizeVector,
       createStack,
-      styles,
-      html,
-      reload,
+      processMarkup,
+      reloadServer,
     )
   );
 };
 
-export const build = gulp.series(
-  clean,
-  copy,
-  optimizeImages,
-  gulp.parallel(
-    styles,
-    html,
-    scripts,
-    optimizeSvg,
-    createStack,
-    createWebp
-  )
-);
+export const buildProd = (done) => {
+  series(
+    removeBuild,
+    copyFiles,
+    optimizeRaster,
+    parallel(
+      processStyles,
+      processMarkup,
+      processScripts,
+      optimizeVector,
+      createStack,
+      createWebp,
+    )
+  )(done);
+}
 
-export default gulp.series(
-  clean,
-  copy,
-  copyImages,
-  gulp.parallel(
-    styles,
-    html,
-    scripts,
-    optimizeSvg,
-    createStack,
-    createWebp
-  ),
-  gulp.series(server, watcher)
-);
+export const runDev = (done) => {
+  series(
+    removeBuild,
+    copyFiles,
+    copyImages,
+    parallel(
+      processStyles,
+      processMarkup,
+      processScripts,
+      optimizeVector,
+      createStack,
+      createWebp
+    ),
+    startServer,
+  )(done);
+}
