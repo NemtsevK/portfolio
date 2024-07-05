186ecd3939883a7f0158a62e843417a05d5195a5
Konstantin <mutecross@gmail.com>
2024-07-05 23:49:16 +0300

    Модальные окна заменены на увеличение блока в grid

--- source/js/main.js
+++ source/js/main.js
@@ -11,4 +11,3 @@ initSkillsContainer();
 initWorksContainer();
 setTheme();
 initModal();
-
--- source/js/works.js
+++ source/js/works.js
@@ -7,43 +7,105 @@ function initWorksContainer() {
   const worksFragment = document.createDocumentFragment();
 
   works.forEach((work) => {
-    const { id, title, description, image, site, technologies, visible } = work;
+    const { title, description, image, about, site, technologies, source } = work;
     const workElement = workItem.cloneNode(true);
-    workElement.querySelector('.works__item-title').innerText = title;
-    workElement.querySelector('.works__item-description').innerText = description;
-    workElement.querySelector('.works__item-image').src = image;
-    workElement.querySelector('.works__item-image').alt = title;
-    workElement.querySelector('.works__item-link').href = site;
-    workElement.querySelector('.works__item-input').value = id;
 
-    if(visible === true) {
-      workElement.classList.add('works__item--hidden');
-    }
+    const workTechnologies = workElement.querySelector('.work__technologies')
+    const workAbout = workElement.querySelector('.work__about')
+    workElement.querySelector('.work__title').innerText = title;
+    workElement.querySelector('.work__description').innerText = description;
+    workElement.querySelector('.work__image').src = image;
+    workElement.querySelector('.work__image').alt = title;
+
+    workElement.querySelector('.work__button--site').href = site;
+    workElement.querySelector('.work__button--source').href = source;
+
+    about.forEach((item) => {
+      const itemElement = document.createElement('p');
+      itemElement.classList.add('work__about-item');
+      itemElement.innerText = item;
+      workAbout.appendChild(itemElement);
+    })
 
     technologies.forEach((technology) => {
       const technologiesItem = document.createElement('li');
-      technologiesItem.classList.add('works__item-technology');
+      technologiesItem.classList.add('work__technology');
       technologiesItem.innerText = technology;
-      workElement.querySelector('.works__item-technologies').appendChild(technologiesItem);
+      workTechnologies.appendChild(technologiesItem);
     })
 
     worksFragment.appendChild(workElement);
   });
 
   worksContainer.appendChild(worksFragment);
+
+  worksContainer.addEventListener('click', (event) => {
+
+
+    const targetElement = event.target;
+    const workElement = targetElement.closest('.work');
+    const workButtonClose = targetElement.closest('.work__button--close');
+
+    if (workButtonClose) {
+      fadeOut(workElement.querySelector('.work__about'), 250);
+      fadeOut(workElement.querySelector('.work__footer'), 250);
+
+      setTimeout(() => {
+        workElement.classList.remove('work--active');
+      }, 250);
+
+      return;
+    }
+
+    if (workElement) {
+      fadeIn(workElement.querySelector('.work__about'), 250);
+      fadeIn(workElement.querySelector('.work__footer'), 250, 'flex');
+
+      const workElementActive = worksContainer.querySelector('.work--active');
+
+      if (workElementActive) {
+        fadeOut(workElementActive.querySelector('.work__about'), 250);
+        fadeOut(workElementActive.querySelector('.work__footer'), 250);
+
+        setTimeout(() => {
+          workElementActive.classList.remove('work--active');
+        }, 250);
+      }
+
+      workElement.classList.add('work--active');
+    }
+  })
 }
 
-const button = document.querySelector('.works__button');
-const worksItems = document.querySelectorAll('.works__item--hide');
 
-let showItems = false;
-
-const onButtonShowClick = () => {
-  worksItems.forEach(item => item.classList.toggle('works__item--hide'));
-  showItems = (showItems === false);
-  button.textContent = (showItems === true) ? 'Скрыть' : 'Показать больше';
+/**
+ * плавное появление элемента
+ * @param element
+ * @param timeout
+ * @param display
+ */
+function fadeIn(element, timeout, display) {
+  element.style.opacity = '0';
+  element.style.display = display || 'block';
+  element.style.transition = `opacity ${timeout}ms`;
+  setTimeout(() => {
+    element.style.opacity = '1';
+  }, 10);
 }
 
-button?.addEventListener('click', onButtonShowClick);
+/**
+ * плавное исчезание элемента
+ * @param element
+ * @param timeout
+ */
+function fadeOut(element, timeout) {
+  element.style.opacity = '1';
+  element.style.transition = `opacity ${timeout}ms`;
+  element.style.opacity = '0';
+
+  setTimeout(() => {
+    element.style.display = 'none'
+  }, timeout);
+}
 
 export { initWorksContainer }
