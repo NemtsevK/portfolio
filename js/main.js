587c179563fc4ac3ab583b0111a0a1cc508dbeb4
Konstantin <mutecross@gmail.com>
2024-07-06 00:09:44 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,1 @@
+import{setTheme}from"./theme.js";import"./nav.js";import{initSkillsContainer,replaceSkillsImage}from"./skills.js";import{initWorksContainer}from"./works.js";import{initModal}from"./modal.js";const page=document.querySelector(".page");page.classList.remove("page--nojs"),initSkillsContainer(),initWorksContainer(),setTheme(),initModal();
